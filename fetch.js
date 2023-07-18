db43ebfe58d570524e125e4e371e20e964b33a73
Vagi-Baweja <92966825+Vagi-Baweja@users.noreply.github.com>
2023-07-18 12:03:16 +0530

    Add files via upload

---
+++ fetch.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        setUsers(xhr.response);
+      }
+    };
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+    xhr.send();
+  }, []);
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            <h2>{user.name}</h2>
+            <p>Email: {user.email}</p>
+            <p>Phone: {user.phone}</p>
+            <p>Website: {user.website}</p>
+            <p>Company: {user.company.name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Users;
